460710255c7b74402377ed6e090a5d5e368d3d22
scarlet-speedster2 <keplar62e@gmail.com>
2023-01-31 11:32:03 +0530

    clean data

--- pro.py
+++ pro.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-df = pd.read_csv("data.csv")
-df.shape()
+df = pd.read_csv('data.csv',sep='|',index_col=False,on_bad_lines='skip',
+                            names=['#','Club','Club.1','Unnamed: 3','W','D','L','Goals','+/-','Pts'])
+
+print(df)
+df.to_csv()
